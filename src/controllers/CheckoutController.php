a695d50abcffb15247b5acd4fac748a4ac77ccf6
Pete Eveleigh <pete@fostercommerce.com>
2024-08-19 15:57:41 +0100

    somehow missed some files in the last commit

--- src/controllers/CheckoutController.php
+++ src/controllers/CheckoutController.php
@@ -5,6 +5,7 @@ namespace fostercommerce\craftfostercheckout\controllers;
 use craft\web\Controller;
 use fostercommerce\craftfostercheckout\FosterCheckout;
 use yii\web\Response;
+use yii\web\Request;
 
 /**
  * Checkout controller
@@ -36,4 +37,5 @@ class CheckoutController extends Controller
             'discounts' => FosterCheckout::getInstance()->checkout->getDiscounts(),
         ]);
     }
+
 }
